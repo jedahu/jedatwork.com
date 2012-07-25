afc62d93e766b1956f6e23d44d45e550415d71c3
Jeremy Hughes <jedahu@gmail.com>
2012-07-26 00:50:50 +1200

    remove boot.sh

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: sh boot.sh $PORT
+web: gunicorn -w -4 -b 0.0.0.0:$PORT -k gevent flog:app
--- boot.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-export FLOG_CONF=`pwd`/flogrc
-echo 'Starting gunicorn,,,'
-exec gunicorn -w 4 -b 0.0.0.0:$1 -k gevent flog:app
